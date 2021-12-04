1d160fb5ef68a174ff8d57cee5f5bb9319397523
Matthew Yeung <matthewteyeung@gmail.com>
2021-12-04 20:18:21 +1100

    test display2

--- src/Components/Display2.js
+++ src/Components/Display2.js
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TotalDeliveryQuantities from "./TotalDeliveryQuantities";
 import TotalAmount from "./TotalAmount";
+import _ from "lodash";
 import "../App.css";
 
-const Display = () => {
-  const [jsonData, setJsonData] = React.useState([]);
+const pageSize = 5;
 
-  React.useEffect(() => {
+const Display = () => {
+  const [jsonData, setJsonData] = useState([]);
+  const [paginate, setPaginate] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+
+  useEffect(() => {
     const fetchJSON = async () => {
       const response = await fetch("/order");
       let json = await response.json();
       setJsonData(json);
-
-      console.log(json);
+      setPaginate(_(json).slice(0).take(pageSize).value());
     };
 
     fetchJSON();
   }, []);
 
+  const pageCount = jsonData ? Math.ceil(jsonData.length / pageSize) : 0;
+  if (pageCount === 1) return null;
+
+  const pages = _.range(1, pageCount + 1);
+
+  const pagination = (page) => {
+    setCurrentPage(page);
+    const startIndex = (page - 1) * pageSize;
+    const paginated = _(jsonData).slice(startIndex).take(pageSize).value();
+    setPaginate(paginated);
+  };
+
   return (
     <div>
+      <div>
+        <div className="navy georgia ma0 grow"></div>
+        <div className="pa2">
+          <input
+            type="search"
+            placeholder="Search"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+      </div>
       <div>
         <table className="table">
           <thead className="theader">
@@ -33,30 +63,58 @@ const Display = () => {
             </tr>
           </thead>
           <tbody>
-            {jsonData.map(function (data, index) {
-              return (
-                <tr key={index}>
-                  {data.orders.map(function (order, orderIndex) {
-                    return (
-                      <tr key={orderIndex}>
-                        <td>{order.order_name}</td>
-                        <td>{data.company_name}</td>
-                        <td>{data.customer_name}</td>
-                        <td>{order.created_at}</td>
-                        <td>
-                          <TotalDeliveryQuantities items={order.OrderItems} />
-                        </td>
-                        <td>
-                          <TotalAmount items={order.OrderItems} />
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tr>
-              );
-            })}
+            {jsonData
+              .filter((val) => {
+                if (search === "") {
+                  return val;
+                } else if (
+                  val.order_name.toLowerCase().includes(search.toLowerCase()) ||
+                  val.company_name
+                    .toLowerCase()
+                    .includes(search.toLowerCase()) ||
+                  val.customer_name.toLowerCase().includes(search.toLowerCase())
+                ) {
+                  return val;
+                }
+              })
+              .map(function (data, index) {
+                return (
+                  <tr key={index}>
+                    <td>{data.order_name}</td>
+                    <td>{data.company_name}</td>
+                    <td>{data.customer_name}</td>
+                    <td>{data.order_date}</td>
+                    <td>
+                      <TotalDeliveryQuantities items={data.order_items} />{" "}
+                    </td>
+                    <td>
+                      <TotalAmount items={data.order_items} />{" "}
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
+        <nav className="d-flex justify-content-center">
+          <ul className="pagination">
+            {pages.map((page) => (
+              <li
+                className={
+                  page === currentPage ? "page-item active" : "page-item"
+                }
+              >
+                <p
+                  className="page-link"
+                  onClick={() => {
+                    pagination(page);
+                  }}
+                >
+                  {page}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </div>
   );
